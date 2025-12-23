27cb0dcbc09c7c6a4273b04e2739464f18b2667e
Eric Zou <ezou626@gmail.com>
2025-12-23 12:57:08 -0500

    start the project with template

---
+++ src/client/java/oobians/mc/SecretPasswordClient.java
@@ -0,0 +1,10 @@
+package oobians.mc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SecretPasswordClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
